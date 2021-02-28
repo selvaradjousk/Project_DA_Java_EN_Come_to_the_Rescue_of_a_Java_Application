3f6877ff880548524fd1e0dfd3d61e848fcf39cf
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-02-28 19:27:11 +0100

    [FEAT]: GenerateOutputReportFile -m  -m  Creating a class GenerateOutputReportFile that performs the tasks to gather the output results computed and provide the results into a output file

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -11,17 +11,10 @@ public class AnalyticsCounter {
 	public static void main(String[] args) throws Exception {
 
 		ReadSymptomDataFromFile.getSymptoms();
-		generateOutputResults();
+		GenerateOutputReportFile.generateOutputResults();
 	}
 
-	// ********* Create a method for Result Output management********
-	private static void generateOutputResults() throws IOException {
-		FileWriter writer = new FileWriter(FileInputAndOutputSourceDefinition.getDataOutputFileName());
-		writer.write("headache: " + ReadSymptomDataFromFile.headacheCount + "\n");
-		writer.write("rash: " + ReadSymptomDataFromFile.rashCount + "\n");
-		writer.write("dialated pupils: " + ReadSymptomDataFromFile.pupilCount + "\n");
-		writer.close();
-	}
+
 
 
 
--- Project02Eclipse/src/com/hemebiotech/analytics/GenerateOutputReportFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/GenerateOutputReportFile.java
@@ -1,5 +1,15 @@
 package com.hemebiotech.analytics;
 
-public class GenerateOutputReportFile {
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class GenerateOutputReportFile {
+	// ********* Create a method for Result Output management********
+	static void generateOutputResults() throws IOException {
+		FileWriter writer = new FileWriter(FileInputAndOutputSourceDefinition.getDataOutputFileName());
+		writer.write("headache: " + ReadSymptomDataFromFile.headacheCount + "\n");
+		writer.write("rash: " + ReadSymptomDataFromFile.rashCount + "\n");
+		writer.write("dialated pupils: " + ReadSymptomDataFromFile.pupilCount + "\n");
+		writer.close();
+	}
 }
