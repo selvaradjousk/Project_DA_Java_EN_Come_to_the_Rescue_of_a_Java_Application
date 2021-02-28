4b8560d3614a3e7b09ea36041ed4f79aecec39ec
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-02-28 20:38:46 +0100

    [PERF]: Implementing  flexible computation feature -m  -m  Adapting flexible computation analytics on the Trend analysis using Array List and Map

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,68 +1,52 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Simple brute force implementation
- *
+ * 
+ * @param filepath a full or partial path to file with symptom strings in it,
+ *                 one per line
  */
-public class ReadSymptomDataFromFile {
+public class ReadSymptomDataFromFile implements ISymptomReader {
 
-//	private String filepath;
+	public String getFilepath() {
+		return filepath;
+	}
 
-	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it,
-	 *                 one per line
-	 */
-//	public ReadSymptomDataFromFile (String filepath) {
-//		this.filepath = filepath;
-//	}
-//	
-//	@Override
-//	public List<String> GetSymptoms() {
-//		ArrayList<String> result = new ArrayList<String>();
-//		
-//		if (filepath != null) {
-//			try {
-//				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-//				String line = reader.readLine();
-//				
-//				while (line != null) {
-//					result.add(line);
-//					line = reader.readLine();
-//				}
-//				reader.close();
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		
-//		return result;
-//	}
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
 
-	static int headacheCount = 0;
-	static int rashCount = 0;
-	static int pupilCount = 0;
+	private String filepath;
 
-	// ********* Create a method for reading input file********
-	static void getSymptoms() throws FileNotFoundException, IOException {
-		BufferedReader reader = new BufferedReader(
-				new FileReader(FileInputAndOutputSourceDefinition.getDataInputFileName()));
-		String line = reader.readLine();
+	public ReadSymptomDataFromFile(String filepath) {
+		this.filepath = filepath;
+	}
 
-		// ********* Method for computation********
-		ComputingSymptomsAnalytics.calculatingSymptomOccurenceFrequencyData(reader, line);
+	@Override
+	public List<String> getSymptoms() {
+		ArrayList<String> result = new ArrayList<String>();
 
-		System.out.println("number of headaches: " + headacheCount);
-		System.out.println("number of rash: " + rashCount);
-		System.out.println("number of pupils: " + pupilCount);
-		reader.close();
+		if (filepath != null) {
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(filepath));
+				String line = reader.readLine();
+
+				while (line != null) {
+					result.add(line);
+					line = reader.readLine();
+				}
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return result;
 	}
 
 }
