c1dea808d636b146d3ccec3cb57ce94b01f20a8d
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-03-02 09:31:34 +0100

    [FEAT]: JavaDoc - Commenting -m  -m  JAVADOC - ReadSymptomDataFromFile Class File

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -6,26 +6,49 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
- * 
- * @param filepath a full or partial path to file with symptom strings in it,
- *                 one per line
+ * ReadSymptomDataFromFile class implements the ISymptomReader interface
+ * and is used to read the symptom data listing in the input source file 
+ * and load it into a List string array 
+ * @author Senthil
+ *
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
+	/**
+	 * The getFilepath() String is set to get the input filename
+	 * @return
+	 */
 	public String getFilepath() {
 		return filepath;
 	}
 
+	/**
+	 * setFilepath() receives the parameter filepath and sets for access to called methods
+	 * @param filepath
+	 */
 	public void setFilepath(String filepath) {
 		this.filepath = filepath;
 	}
 
 	private String filepath;
 
+	/**
+	 * ReadSymptomDataFromFile() class get filepath as an input parameter
+	 * @param filepath
+	 */
 	public ReadSymptomDataFromFile(String filepath) {
 		this.filepath = filepath;
 	}
+
+	/**
+	 * getSymptoms() function loads the symptoms listed in input source file
+	 * and is accessible in the program calling the function referencing 
+	 * ReadSymptomDataFromFile class
+	 * @return result  The ArrayList is returned by the function on calling
+	 */
+
 
 	@Override
 	public List<String> getSymptoms() {
