d0330b56bffc006542c2e033a832cc1b0025de78
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-03-02 20:39:23 +0100

    introducing Javadoc comments and refactoring the code

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -8,32 +8,45 @@ import java.util.TreeMap;
 
 public class AnalyticsCounter {
 
-	
 	static List<String> listedSymptomOccurenceInSourceData = new ArrayList<String>();
-	
+
 	static Map<String, Integer> loadingSymptomsIntoMap = new TreeMap<String, Integer>();
-	
+
+	/**
+	 * readInputFile() calls the functions that reads the source input file and
+	 * loads the symptoms into array list
+	 */
 	public static void readInputFile() {
 		ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile(
 				FileInputAndOutputSourceDefinition.getDataInputFileName());
 		listedSymptomOccurenceInSourceData = readSymptomDataFromFile.getSymptoms();
 	}
-	
-	public static void symptomOccurenceCount() {	
-	try {
-		loadingSymptomsIntoMap = ComputingSymptomsAnalytics.sortedSymptomOccurenceFrequencyData(listedSymptomOccurenceInSourceData);
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+
+	/**
+	 * symptomOccurenceCount() used to call the functions for calculating the
+	 * symptoms occurrence frequency and sorting them into TreeMap to provide sorted
+	 * results of the symptoms and its counts
+	 */
+	public static void symptomOccurenceCount() {
+		try {
+			loadingSymptomsIntoMap = ComputingSymptomsAnalytics
+					.sortedSymptomOccurenceFrequencyData(listedSymptomOccurenceInSourceData);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	}
-	
+
+	/**
+	 * sendResultToOutputFile() used to call the functions to that sends the count
+	 * frequency computed to output file
+	 */
 	public static void sendResultToOutputFile() {
-	try {
-		GenerateOutputReportFile.generateOutputResults(loadingSymptomsIntoMap, listedSymptomOccurenceInSourceData);
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+		try {
+			GenerateOutputReportFile.generateOutputResults(loadingSymptomsIntoMap, listedSymptomOccurenceInSourceData);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
